b90db75113fd133490d9e3e0c9a8c340e5ccc843
Sara Ibarra <saraiba33@gmail.com>
2022-05-24 09:28:23 -0600

    Refactor solution

--- MathApp.java
+++ MathApp.java
@@ -1,39 +1,44 @@
-import java.util.Random;
-
 public class MathApp {
   public static void main(String[] args) {
+    System.out.println("The best salary is $" + compareSalary(43021.23, 60790.56));
+    System.out.println("The lowest vehicle price $" + compareCarPrice(23450, 45089));
+    System.out.println("The area of the circle is " + String.format("%.2f", areaOfCircle(7.25)));
+    System.out.println("The squareroot is " + String.format("%.2f", squareRoot(5.0)));
+    System.out.println("The distance between is " + String.format("%.2f", findTheDistance(5, 85, 10, 50)));
+    System.out.println("The absolute number is " + absoluteNumber(-3.8));
+    System.out.println("Random number between 0 & 1: " + String.format("%.2f", randomNumber()));
 
-    double job1Salary = 42340.12;
-    double job2Salary = 51015.43;
-    double whichIsHigher = Math.max(job1Salary, job2Salary);
-    System.out.println("This is the best salary: $" + whichIsHigher);
+  }
 
-    int carPrice = 28000;
-    int truckPrice = 45000;
-    int whichIsLower = Math.min(carPrice, truckPrice);
-    System.out.println("This is the lowest price: $" + whichIsLower);
+  private static double compareSalary(double job1Salary, double job2Salary) {
+    return Math.max(job1Salary, job2Salary);
+  }
 
-    double radiusSquared = 7.25 * 7.25;
-    double area = Math.PI * radiusSquared;
-    System.out.println("The area of the circle is: " + String.format("%.2f", area));
+  private static int compareCarPrice(int carPrice, int truckPrice) {
+    return Math.min(carPrice, truckPrice);
 
-    double num1 = 5.0;
-    double squareRoot = Math.sqrt(num1);
-    System.out.println(String.format("%.2f", squareRoot));
+  }
 
-    int x1 = 5;
-    int y1 = 10;
-    int x2 = 85;
-    int y2 = 50;
-    double distance = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    System.out.println("The distance between is " + String.format("%.2f", distance));
+  private static double areaOfCircle(double radius) {
+    double radiusSquared = radius * radius;
+    return Math.PI * radiusSquared;
+  }
 
-    double num2 = -3.8;
-    System.out.println(Math.abs(num2));
+  private static double squareRoot(double num1) {
+    return Math.sqrt(num1);
+  }
 
-    Random random = new Random();
-    int randomNumber = random.nextInt(10);
-    System.out.println("A random number " + randomNumber);
+  private static double findTheDistance(int x1, int x2, int y1, int y2) {
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  }
+
+  private static double absoluteNumber(double num2) {
+    return Math.abs(num2);
+
+  }
+
+  private static double randomNumber() {
+    return Math.random();
 
   }
 }
